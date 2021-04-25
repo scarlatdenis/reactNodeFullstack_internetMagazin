a2525c00c086235d7bfad978bc8f1a671e6c1703
denis <denis.scarlat@gmail.com>
2021-04-25 22:23:20 +0300

    25.09 connected to server succesifuly

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "WORKING!!!" });
+
+// });
 
 const start = async () => {
   try {
@@ -16,6 +25,5 @@ const start = async () => {
     console.log(e);
   }
 };
-
 
 start();
