aed0d9ad26e28c77f5acb1059ecfe1f61d0ee2e1
denis <denis.scarlat@gmail.com>
2021-05-01 17:08:10 +0300

    36.50 added middleware error

---
+++ server/controllers/brandController.js
@@ -0,0 +1,8 @@
+class BrandController {
+    async create(req, res) {}
+    async getAll(req, res) {}
+  }
+  
+  module.exports = new BrandController();
+  
+  
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
 const router = require("./routes/index");
+const errorHandler = require("./middleWare/ErrorHandingMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+
+
+// Error handler need to be last
+app.use(errorHandler);
 
 // app.get("/", (req, res) => {
 //   res.status(200).json({ message: "WORKING!!!" });
