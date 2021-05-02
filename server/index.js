4f470c2bdbde1ba9b08b476815a73624eadda220
denis <denis.scarlat@gmail.com>
2021-05-02 19:00:19 +0300

    41.16 commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const errorHandler = require("./middleWare/ErrorHandingMiddleware");
 
@@ -12,8 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use("/api", router);
-
 
 // Error handler need to be last
 app.use(errorHandler);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const Router = require("express");
 const router = new Router();
 
-const deviceRouter = require("./userRouter");
+const deviceRouter = require("./deviceRouter");
 const userRouter = require("./userRouter");
 const brandRouter = require("./brandRouter");
 const typeRouter = require("./typeRouter");
@@ -9,6 +9,6 @@ const typeRouter = require("./typeRouter");
 router.use("/user", userRouter);
 router.use("/type", typeRouter);
 router.use("/brand", brandRouter);
-router.use("/deice", deviceRouter);
+router.use("/device", deviceRouter);
 
 module.exports = router; 
